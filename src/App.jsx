738e2838a4166b7163ea9494fdb1c2ac56b8dc73
Joao Felipe Lemos <lemos391@gmail.com>
2023-04-13 18:12:30 -0300

    Fisrt Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { useState } from 'react'
+import Hello from './components/Hello'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <Hello />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Hello.jsx
@@ -0,0 +1,11 @@
+const Hello = () => {
+  return (
+    <div>
+        <h1>Hello!</h1>
+        <h2>Em breve teremos uma aplicação bem legal aqui ;)</h2>
+        <img src="https://github.com/joaofelipelemos.png" alt="" />
+    </div>
+  )
+}
+
+export default Hello
